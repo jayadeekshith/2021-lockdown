1a2ee724aea2ed7f0c2a041467e71d6fcae078c9
jayadeekshith <82016666+jayadeekshith@users.noreply.github.com>
2021-06-24 23:06:40 +0530

    Update finding same sub string.cpp

--- finding same sub string.cpp
+++ finding same sub string.cpp
@@ -28,3 +28,25 @@ sort(a,a+n1);
 cout<<a[0];
 return 0;
 }
+////// or////////
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1;
+    string s2;
+    cin>>s1>>s2;
+    int l,i;
+    n=s1.length();
+    int v[n];
+    for( i=0;i<n;i++)
+    {
+        char c;
+        c=s1[i];
+        v[i]=count(s2.begin(),s2.end(),c);
+    }
+    cout<<*min_element(v,v+n);
+    return 0;
+}

4667825d112ca76fac400bf85cb81a87e8b402ba
Jaya Deekshith <82016666+jayadeekshith@users.noreply.github.com>
2021-07-13 21:33:47 +0530

    Create even or odd.cpp

---
+++ even or odd.cpp
@@ -0,0 +1,54 @@
+#way 1 using ~
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+ll t;
+cin>>t;
+for(int i=2;i<=t;i=(-(~i))+1){
+cout<<i<<" ";
+}
+return 0;
+
+    
+}
+
+#way 2 using %2==0
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+ll t;
+cin>>t;
+for(int i=2;i<=t;i++){
+if(i%2==0)
+{
+  cout<<i<<" ";
+}
+}
+return 0;
+
+    
+}
+
+
+
+#way 3 using +=2
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+ll t;
+cin>>t;
+for(int i=2;i<=t;i+=1){
+cout<<i<<" ";
+}
+return 0;
+
+    
+}
